854152d0f50dd29fc366cca3aa47574204fcfec5
chakku <chakkurai0724@outlook.jp>
2018-07-21 16:54:17 +0900

    masterにごめん

---
+++ chakku/yu.sh
@@ -0,0 +1,4 @@
+echo $1
+OCAMLRUNPARAM='l=4G' ../kuwa/1/decode_trace ../trace/LA$1.nbt > input/LA$1.txt
+./src/a.out ./input/LA$1.txt > output/LA$1.txt
+OCAMLRUNPARAM='l=4G' ../kuwa/1/encode_trace output/LA$1.txt > submit/LA$1.nbt
