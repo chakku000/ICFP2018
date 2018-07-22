dcfc73414cb5e3e65b155e9814c5c55d6384fc58
chakku <chakkurai0724@outlook.jp>
2018-07-22 14:54:12 +0900

    ぽい

--- chakku/yu.sh
+++ chakku/yu.sh
@@ -1,4 +1,5 @@
 echo $1
-#OCAMLRUNPARAM='l=4G' ../kuwa/1/decode_trace ../trace/LA$1.nbt > input/LA$1.txt
-./src/a.out ./input/LA$1.txt ../models/LA$1_tgt.mdl > output/LA$1.txt
-OCAMLRUNPARAM='l=4G' ../kuwa/1/encode_trace output/LA$1.txt > submit/LA$1.nbt
+#OCAMLRUNPARAM='l=4G' ../kuwa/1/decode_trace  ./../../ICFPC/defaultTraceF/FA$1.nbt > input/FA/FA$1.txt
+echo $HOME/Programing/CompetitivePrograming/ICFPC/problemF/FA${1}_tgt.mdl
+./src/a.out ./input/FA/FA${1}.txt $HOME/Programing/CompetitivePrograming/ICFPC/problemF/FA${1}_tgt.mdl > output/FA/FA${1}.txt
+OCAMLRUNPARAM='l=4G' $HOME/Programing/CompetitivePrograming/ICFP2018/kuwa/1/encode_trace $HOME/Programing/CompetitivePrograming/ICFP2018/chakku/output/FA/FA${1}.txt > submit/FA/FA${1}.nbt
