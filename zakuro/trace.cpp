b780ecc0514eb40bc49c35ba565bfb0575262ffc
45deg <45deg@users.noreply.github.com>
2018-07-22 14:42:13 +0900

    Updated for Full

--- zakuro/trace.cpp
+++ zakuro/trace.cpp
@@ -11,12 +11,16 @@
 #define OP_FUSS 7
 #define OP_FISS 8
 #define OP_FILL 9
+#define OP_VOID 10
+#define OP_GFIL 11
+#define OP_GVID 12
 
 union command_data {
   struct { int x, y, z; } lld;
   struct { int x1, y1, z1; int x2, y2, z2; } sld;
   struct { int x, y, z; } nd;
   struct { int x, y, z; int m; } ndm;
+  struct { int nx, ny, nz; int fx, fy, fz; } nfd;
 };
 struct command {
   int type;
@@ -34,6 +38,7 @@ command_data decode_lld(unsigned char a, unsigned char i){
     case 0b11:
       return { .lld = {0, 0, (int)i-15} };
   }
+  throw std::runtime_error("WTF");
 }
 
 command_data decode_sld(unsigned char a1, unsigned char i1, unsigned char a2, unsigned char i2){
@@ -54,6 +59,17 @@ command_data decode_nd(unsigned char nd, int m){
   } else {
     return { .ndm = { (int)nd/9 - 1, (int)(nd % 9) / 3 - 1, (int)(nd % 3) - 1 , m } };
   }
+}
+
+command_data decode_ndf(unsigned char nd, unsigned char fx, unsigned char fy, unsigned char fz){
+  return {.nfd = {
+              (int)nd / 9 - 1,
+              (int)(nd % 9) / 3 - 1,
+              (int)(nd % 3) - 1,
+              (int)fx - 30,
+              (int)fy - 30,
+              (int)fz - 30,
+          }};
 }
 
 std::vector<command> decode(std::ifstream& fin){
@@ -105,8 +121,25 @@ std::vector<command> decode(std::ifstream& fin){
     } else if((c & 0b111) == 0b011){
       unsigned char nd = (c >> 3) & 0b11111;
       command_data ndd = decode_nd(nd, -1);
-      //std::cout << "Fil" << ndd.nd.x << ndd.nd.y << ndd.nd.z << std::endl;
       cmds.push_back(command(OP_FILL, ndd));
+    } else if((c & 0b111) == 0b010){
+      unsigned char nd = (c >> 3) & 0b11111;
+      command_data ndd = decode_nd(nd, -1);
+      cmds.push_back(command(OP_VOID, ndd));
+    } else if((c & 0b111) == 0b001){
+      unsigned char nd = (c >> 3) & 0b11111;
+      unsigned char fx = fin.get();
+      unsigned char fy = fin.get();
+      unsigned char fz = fin.get();
+      command_data ndd = decode_ndf(nd, fx, fy, fz);
+      cmds.push_back(command(OP_GFIL, ndd));
+    } else if((c & 0b111) == 0b000){
+      unsigned char nd = (c >> 3) & 0b11111;
+      unsigned char fx = fin.get();
+      unsigned char fy = fin.get();
+      unsigned char fz = fin.get();
+      command_data ndd = decode_ndf(nd, fx, fy, fz);
+      cmds.push_back(command(OP_GVID, ndd));
     }
   }
   return cmds;
@@ -152,6 +185,57 @@ void encode(std::vector<command> cmds, std::ofstream& fout){
     } else if(t == OP_FILL){
       unsigned char nd = (d.nd.x + 1) * 9 + (d.nd.y + 1) * 3 + (d.nd.z + 1);
       fout.put((nd << 3) | 0b011);
+    } else if(t == OP_VOID) {
+      unsigned char nd = (d.nd.x + 1) * 9 + (d.nd.y + 1) * 3 + (d.nd.z + 1);
+      fout.put((nd << 3) | 0b010);
+    } else if(t == OP_GFIL) {
+      unsigned char nd = (d.nfd.nx + 1) * 9 + (d.nfd.ny + 1) * 3 + (d.nfd.nz + 1);
+      fout.put((nd << 3) | 0b001);
+      fout.put(d.nfd.fx & 0xFF);
+      fout.put(d.nfd.fy & 0xFF);
+      fout.put(d.nfd.fz & 0xFF);
+    } else if(t == OP_GFIL) {
+      unsigned char nd = (d.nfd.nx + 1) * 9 + (d.nfd.ny + 1) * 3 + (d.nfd.nz + 1);
+      fout.put((nd << 3) | 0b000);
+      fout.put(d.nfd.fx & 0xFF);
+      fout.put(d.nfd.fy & 0xFF);
+      fout.put(d.nfd.fz & 0xFF);
+    }
+  }
+}
+
+void print(std::vector<command> cmds){
+  for(command& cmd: cmds){
+    int t = cmd.type;
+    command_data& d = cmd.data;
+    if(t == OP_HALT) {
+      std::cout << "Halt" << std::endl;
+    } else if(t == OP_WAIT) {
+      std::cout << "Wait" << std::endl;
+    } else if(t == OP_FLIP) {
+      std::cout << "Flip" << std::endl;
+    } else if(t == OP_SMOV) {
+      std::cout << "SMove <" << d.lld.x << "," << d.lld.y << "," << d.lld.z << ">" << std::endl;
+    } else if(t == OP_LMOV) {
+      std::cout << "LMove <" << d.sld.x1 << "," << d.sld.y1 << "," << d.sld.z1 << ">" 
+                    << ", <" << d.sld.x2 << "," << d.sld.y2 << "," << d.sld.z2 << ">" << std::endl;
+    } else if(t == OP_FUSP){
+      std::cout << "FusionP <" << d.nd.x << "," << d.nd.y << "," << d.nd.z << ">" << std::endl;
+    } else if(t == OP_FUSS){
+      std::cout << "FusionS <" << d.nd.x << "," << d.nd.y << "," << d.nd.z << ">" << std::endl;
+    } else if(t == OP_FISS){
+      std::cout << "Fission <" << d.ndm.x << "," << d.ndm.y << "," << d.ndm.z << "> " << d.ndm.m << std::endl;
+    } else if(t == OP_FILL){
+      std::cout << "Fill <" << d.nd.x << "," << d.nd.y << "," << d.nd.z << ">" << std::endl;
+    } else if(t == OP_VOID) {
+      std::cout << "Void <" << d.nd.x << "," << d.nd.y << "," << d.nd.z << ">" << std::endl;
+    } else if(t == OP_GFIL) {
+      std::cout << "GFill <" << d.nfd.nx << "," << d.nfd.ny << "," << d.nfd.nz << ">" 
+                    << ", <" << d.nfd.fx << "," << d.nfd.fy << "," << d.nfd.fz << ">" << std::endl;
+    } else if(t == OP_GFIL) {
+      unsigned char nd = (d.nfd.nx + 1) * 9 + (d.nfd.ny + 1) * 3 + (d.nfd.nz + 1);
+      std::cout << "Gvoid <" << d.nfd.nx << "," << d.nfd.ny << "," << d.nfd.nz << ">" 
+                    << ", <" << d.nfd.fx << "," << d.nfd.fy << "," << d.nfd.fz << ">" << std::endl;
     }
   }
 }
@@ -160,6 +244,7 @@ void encode(std::vector<command> cmds, std::ofstream& fout){
 int main(int argc, char* argv[]) {
   std::ifstream fin(argv[1], std::ios::binary);
   std::ofstream fout(argv[2], std::ios::binary);
-  encode(decode(fin), fout);
+  auto codes = decode(fin);
+  print(codes);
   fin.close();
 }
